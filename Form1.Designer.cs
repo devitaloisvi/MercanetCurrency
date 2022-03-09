30643681da6a09f6e1b45e53dd698c22bafb57bc
Italo Isvi <Italo Isvi@ISVI>
2022-03-09 19:40:56 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace MercanetCurrency
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Novo = new System.Windows.Forms.Button();
+            this.Salvar = new System.Windows.Forms.Button();
+            this.Excluir = new System.Windows.Forms.Button();
+            this.Codigo = new System.Windows.Forms.Label();
+            this.descricao = new System.Windows.Forms.Label();
+            this.abreviatura = new System.Windows.Forms.Label();
+            this.cotacao = new System.Windows.Forms.Label();
+            this.txt_codigo = new System.Windows.Forms.TextBox();
+            this.txt_descricao = new System.Windows.Forms.TextBox();
+            this.txt_abreviatura = new System.Windows.Forms.TextBox();
+            this.txt_cotacao = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Novo
+            // 
+            this.Novo.Location = new System.Drawing.Point(13, 13);
+            this.Novo.Name = "Novo";
+            this.Novo.Size = new System.Drawing.Size(75, 23);
+            this.Novo.TabIndex = 0;
+            this.Novo.Text = "NOVO";
+            this.Novo.UseVisualStyleBackColor = true;
+            this.Novo.Click += new System.EventHandler(this.Novo_Click);
+            // 
+            // Salvar
+            // 
+            this.Salvar.Location = new System.Drawing.Point(118, 13);
+            this.Salvar.Name = "Salvar";
+            this.Salvar.Size = new System.Drawing.Size(75, 23);
+            this.Salvar.TabIndex = 1;
+            this.Salvar.Text = "SALVAR";
+            this.Salvar.UseVisualStyleBackColor = true;
+            this.Salvar.Click += new System.EventHandler(this.Salvar_Click);
+            // 
+            // Excluir
+            // 
+            this.Excluir.Location = new System.Drawing.Point(218, 13);
+            this.Excluir.Name = "Excluir";
+            this.Excluir.Size = new System.Drawing.Size(75, 23);
+            this.Excluir.TabIndex = 2;
+            this.Excluir.Text = "EXCLUIR";
+            this.Excluir.UseVisualStyleBackColor = true;
+            this.Excluir.Click += new System.EventHandler(this.Excluir_Click);
+            // 
+            // Codigo
+            // 
+            this.Codigo.AutoSize = true;
+            this.Codigo.Location = new System.Drawing.Point(13, 94);
+            this.Codigo.Name = "Codigo";
+            this.Codigo.Size = new System.Drawing.Size(40, 13);
+            this.Codigo.TabIndex = 3;
+            this.Codigo.Text = "Código";
+            // 
+            // descricao
+            // 
+            this.descricao.AutoSize = true;
+            this.descricao.Location = new System.Drawing.Point(13, 145);
+            this.descricao.Name = "descricao";
+            this.descricao.Size = new System.Drawing.Size(55, 13);
+            this.descricao.TabIndex = 4;
+            this.descricao.Text = "Descrição";
+            // 
+            // abreviatura
+            // 
+            this.abreviatura.AutoSize = true;
+            this.abreviatura.Location = new System.Drawing.Point(13, 197);
+            this.abreviatura.Name = "abreviatura";
+            this.abreviatura.Size = new System.Drawing.Size(61, 13);
+            this.abreviatura.TabIndex = 5;
+            this.abreviatura.Text = "Abreviatura";
+            // 
+            // cotacao
+            // 
+            this.cotacao.AutoSize = true;
+            this.cotacao.Location = new System.Drawing.Point(13, 250);
+            this.cotacao.Name = "cotacao";
+            this.cotacao.Size = new System.Drawing.Size(47, 13);
+            this.cotacao.TabIndex = 6;
+            this.cotacao.Text = "Cotação";
+            // 
+            // txt_codigo
+            // 
+            this.txt_codigo.Location = new System.Drawing.Point(118, 91);
+            this.txt_codigo.Name = "txt_codigo";
+            this.txt_codigo.Size = new System.Drawing.Size(100, 20);
+            this.txt_codigo.TabIndex = 7;
+            // 
+            // txt_descricao
+            // 
+            this.txt_descricao.Location = new System.Drawing.Point(118, 137);
+            this.txt_descricao.Name = "txt_descricao";
+            this.txt_descricao.Size = new System.Drawing.Size(175, 20);
+            this.txt_descricao.TabIndex = 8;
+            // 
+            // txt_abreviatura
+            // 
+            this.txt_abreviatura.Location = new System.Drawing.Point(118, 189);
+            this.txt_abreviatura.Name = "txt_abreviatura";
+            this.txt_abreviatura.Size = new System.Drawing.Size(100, 20);
+            this.txt_abreviatura.TabIndex = 9;
+            // 
+            // txt_cotacao
+            // 
+            this.txt_cotacao.Location = new System.Drawing.Point(118, 242);
+            this.txt_cotacao.Name = "txt_cotacao";
+            this.txt_cotacao.Size = new System.Drawing.Size(100, 20);
+            this.txt_cotacao.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txt_cotacao);
+            this.Controls.Add(this.txt_abreviatura);
+            this.Controls.Add(this.txt_descricao);
+            this.Controls.Add(this.txt_codigo);
+            this.Controls.Add(this.cotacao);
+            this.Controls.Add(this.abreviatura);
+            this.Controls.Add(this.descricao);
+            this.Controls.Add(this.Codigo);
+            this.Controls.Add(this.Excluir);
+            this.Controls.Add(this.Salvar);
+            this.Controls.Add(this.Novo);
+            this.Name = "Form1";
+            this.Text = "Cadastro de moedas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Novo;
+        private System.Windows.Forms.Button Salvar;
+        private System.Windows.Forms.Button Excluir;
+        private System.Windows.Forms.Label Codigo;
+        private System.Windows.Forms.Label descricao;
+        private System.Windows.Forms.Label abreviatura;
+        private System.Windows.Forms.Label cotacao;
+        private System.Windows.Forms.TextBox txt_codigo;
+        private System.Windows.Forms.TextBox txt_descricao;
+        private System.Windows.Forms.TextBox txt_abreviatura;
+        private System.Windows.Forms.TextBox txt_cotacao;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace MercanetCurrency
+{
+    public partial class Form1 : Form
+    {        
+        public Form1()
+        {
+            InitializeComponent();
+
+            txt_codigo.Enabled = false;
+            txt_descricao.Enabled = false;
+            txt_abreviatura.Enabled = false;
+            txt_cotacao.Enabled = false;
+
+            txt_codigo.Clear();
+            txt_descricao.Clear();
+            txt_abreviatura.Clear();
+            txt_cotacao.Clear();
+        }        
+
+        SqlConnection sqlconn = null;
+        private readonly string stgCon = @"Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=MERCANET_DB;Data Source=(localdb)\MSSQLLocalDB";
+        private string stgSql = string.Empty;
+
+        private void Novo_Click(object sender, EventArgs e)
+        {
+            txt_codigo.Enabled = true;
+            txt_descricao.Enabled = true;
+            txt_abreviatura.Enabled = true;
+            txt_cotacao.Enabled = true;
+
+            txt_codigo.Clear();
+            txt_descricao.Clear();
+            txt_abreviatura.Clear();
+            txt_cotacao.Clear();
+        }
+
+        public class AutoClosingMessageBox
+        {
+            readonly System.Threading.Timer timeoutTimer;
+            readonly string caption;
+            AutoClosingMessageBox(string text, string caption, int timeout)
+            {
+                this.caption = caption;
+                timeoutTimer = new System.Threading.Timer(OnTimerElapsed,
+                    null, timeout, System.Threading.Timeout.Infinite);
+                using (timeoutTimer)
+                    MessageBox.Show(text, caption);
+            }
+            public static void Show(string text, string caption, int timeout)
+            {
+                new AutoClosingMessageBox(text, caption, timeout);
+            }
+            void OnTimerElapsed(object state)
+            {
+                IntPtr mbWnd = FindWindow("#32770", caption);
+                if (mbWnd != IntPtr.Zero)
+                    SendMessage(mbWnd, WM_CLOSE, IntPtr.Zero, IntPtr.Zero);
+                timeoutTimer.Dispose();
+            }
+            const int WM_CLOSE = 0x0010;
+            [System.Runtime.InteropServices.DllImport("user32.dll", SetLastError = true)]
+            static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+            [System.Runtime.InteropServices.DllImport("user32.dll", CharSet = System.Runtime.InteropServices.CharSet.Auto)]
+            static extern IntPtr SendMessage(IntPtr hWnd, UInt32 Msg, IntPtr wParam, IntPtr lParam);
+        }
+
+
+
+        private void Salvar_Click(object sender, EventArgs e)
+        {
+            
+
+            stgSql = "insert into cadastro (codigo, descricao, abreviatura, cotacao) values ('" + txt_codigo.Text + "', '" + txt_descricao.Text + "', '" + txt_abreviatura.Text + "', '" + txt_cotacao.Text + "')";
+            sqlconn = new SqlConnection(stgCon);
+
+            SqlCommand comando = new SqlCommand(stgSql, sqlconn);
+
+            comando.Parameters.Add("@codigo", SqlDbType.VarChar).Value = stgSql;
+            comando.Parameters.Add("@descricao", SqlDbType.VarChar).Value = stgSql;
+            comando.Parameters.Add("@abreviatura", SqlDbType.VarChar).Value = stgSql;
+            comando.Parameters.Add("@cotacao", SqlDbType.VarChar).Value = stgSql;
+
+            if(txt_codigo.Text == String.Empty)
+            {
+                MessageBox.Show("PREENCHA O CAMPO 'Código'.");
+                return;
+            } else if(txt_descricao.Text == String.Empty)
+            {
+                MessageBox.Show("PREENCHA O CAMPO 'Descrição'.");
+                return;
+            }
+
+            try
+            {
+                sqlconn.Open();
+                comando.ExecuteNonQuery();                
+                Form1.AutoClosingMessageBox.Show("MOEDA SALVA COM SUCESSO!", "", 4000);
+            }
+
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            finally
+            {
+                sqlconn.Close();
+            }
+        }
+
+        private void Excluir_Click(object sender, EventArgs e)
+        {
+            stgSql = "delete from Cadastro where codigo=" + txt_codigo.Text;
+            sqlconn = new SqlConnection(stgCon);
+            SqlCommand comando = new SqlCommand(stgSql, sqlconn);
+
+            comando.Parameters.Add("@codigo", SqlDbType.VarChar).Value = stgSql;
+
+            try
+            {
+                sqlconn.Open();
+                comando.ExecuteNonQuery();
+                AutoClosingMessageBox.Show("MOEDA EXCLUÍDA COM SUCESSO!", "", 4000);
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            finally
+            {
+                sqlconn.Close();
+            }
+
+            txt_codigo.Enabled = true;
+            txt_descricao.Enabled = true;
+            txt_abreviatura.Enabled = true;
+            txt_cotacao.Enabled = true;
+
+            txt_codigo.Clear();
+            txt_descricao.Clear();
+            txt_abreviatura.Clear();
+            txt_cotacao.Clear();
+        }
+    }
+}
